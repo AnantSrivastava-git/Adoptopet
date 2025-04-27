078f627bc0c7d93cac381f2350d12e59b387e77f
AnantSrivastava-git <anantsrivastava65@gmail.com>
2025-04-28 02:08:29 +0530

    Delete adoption-page.js
    Signed-off-by: AnantSrivastava-git <anantsrivastava65@gmail.com>

--- adoption-page.js
+++
@@ -1,9 +0,0 @@
-const menuBtn = document.getElementById('menu-button');
-const sidebar = document.getElementById('sidebar');
-
-function sidebartoggle(){
-  const expanded = menuBtn.getAttribute('aria-expanded') === 'true';
-  menuBtn.setAttribute('aria-expanded', !expanded);
-  sidebar.classList.toggle('active');
-  sidebar.setAttribute('aria-hidden', expanded);
-};
